36441439ac9bc1576f381c0a6c9fc4f0c3bb0fbd
jmrad <julien.mrad@gmail.com>
2015-03-10 11:22:42 +0200

    test

---
+++ scripts/test.js
@@ -0,0 +1,9 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ var l = require("log");
+l.setLevel("debug");
+l.info("{ \"<b>name</b>\" : \"julien\" }");
+return { "name" : "julien" };   							

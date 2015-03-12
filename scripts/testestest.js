4121bf278ea4b9625e8113071cbf3e659d4f627e
jmrad <julien.mrad@gmail.com>
2015-03-12 16:30:33 +0200

    big script

---
+++ scripts/testestest.js
@@ -0,0 +1,56 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+a = 1;
+
+return a;   							

5a793f5928865aeeddbae91de6cd5d6ff79a1e58
jmrad <julien.mrad@gmail.com>
2015-03-13 23:38:54 +0200

    ZX

--- newt/scripts/newty.js
+++ newt/scripts/newty.js
@@ -1,7 +1,7 @@
 /** Script ACLs do not delete 
  read=nobody 
 write=nobody
-execute=authenticated 
+execute=anonymous 
   **/ 
- 
- return "new test1";   							
+ return "new tssdsest1dfdg";   							
+   							

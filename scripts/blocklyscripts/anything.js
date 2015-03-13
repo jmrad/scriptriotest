0ab4eac5e2d0f9dacbc00947fc68fb6185118717
jmrad <julien.mrad@gmail.com>
2015-03-14 00:39:42 +0200

    asdf

---
+++ scripts/blocklyscripts/anything.js
@@ -0,0 +1,9 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=anonymous 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="controls_repeat_ext" id="10" inline="true" x="773" y="55"><value name="TIMES"><block type="math_number" id="11"><field name="NUM">10</field></block></value></block></xml>*#*#*/
+for (var count = 0; count < 10; count++) {
+}
+   							

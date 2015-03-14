69773b26255ba1f818a3230e15347ea58aeb56b2
jmrad <julien.mrad@gmail.com>
2015-03-14 17:52:29 +0200

    using /** **/

---
+++ scripts/weirdtest.js
@@ -0,0 +1,11 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=anonymous 
+  **/ 
+ /** this is
+a blockly
+script
+**/
+if (0 == '0') {    
+  return 'blockly compares values without variable types'}   				   							

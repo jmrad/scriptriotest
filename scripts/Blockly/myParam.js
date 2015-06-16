9c4795a7352b605a566a519ec5b8266bcd6ec47c
jmrad <julien.mrad@gmail.com>
2015-06-16 15:32:53 +0300

    asdf

--- scripts/Blockly/myParam.js
+++ scripts/Blockly/myParam.js
@@ -3,5 +3,6 @@
 write=nobody
 execute=authenticated 
   **/ 
+ 
  /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="13" inline="false" x="31" y="25"><value name="return"><block type="scriptr_params" id="26"><field name="parameter">myParam</field></block></value></block></xml>*#*#*/
 return request.parameters["myParam"]   							

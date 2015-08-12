f1faa222dc5ac9e6a8b249ee0bd6d30610f52960
jmrad <julien.mrad@gmail.com>
2015-08-12 19:26:39 +0300

    minor folder restructuring

--- scripts/messaging/channel/get/valid.js
+++ scripts/messaging/channel/get/valid.js
@@ -9,7 +9,7 @@ var log = require("log");
 log.setLevel("DEBUG");
 
 var messaging = require("messaging");
-var result = messaging.channel.delete(id);
+var result = messaging.channel.get(id);
 log.debug(result);
 
-return result;     				   							   				   				   							
+return result;   				   				   				   							   							

af7afb9dfa23f9da5345b432a2208237fcf5786d
jmrad <julien.mrad@gmail.com>
2015-08-14 10:38:46 +0300

    implement woopra and google analytics modules

---
+++ scripts/woopra/config.js
@@ -0,0 +1,23 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ // woopra track url 
+var woopra_http_tracking_url = "http://www.woopra.com/track/ce";
+// woopra identify url 
+var woopra_http_identify_url = "http://www.woopra.com/track/identify";
+var timeout="300000";
+
+
+function getTimeout() {
+  return timeout;
+}
+
+function getWoopraHttpTrackingURL() {
+  return woopra_http_tracking_url;
+}
+   	
+function getWoopraHttpIdentifyURL() {
+  return woopra_http_identify_url;
+}   				   							

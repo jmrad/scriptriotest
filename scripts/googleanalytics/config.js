3ff5a11d6872cba1d2fec3e69489ecf24a82c00b
jmrad <julien.mrad@gmail.com>
2015-08-27 18:05:11 +0300

    Update config.js

--- scripts/googleanalytics/config.js
+++ scripts/googleanalytics/config.js
@@ -1,10 +1,4 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- 
- // google analytics debugging collect url 
+// google analytics debugging collect url 
 var analyticsDebugUrl="https://www.google-analytics.com/debug/collect";
 // google analytics collect url 
 var analyticsUrl="https://www.google-analytics.com/collect";

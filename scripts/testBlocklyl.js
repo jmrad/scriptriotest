87b1415e0e3f888ecdb36f3aa6dd57f07d62f3f2
jmrad <julien.mrad@gmail.com>
2015-10-02 12:17:47 +0300

    ww

--- scripts/testBlocklyl.js
+++ scripts/testBlocklyl.js
@@ -3,6 +3,7 @@
 write=nobody
 execute=authenticated 
   **/ 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="1" inline="false" x="27" y="53"><value name="return"><block type="scriptr_fb_post" id="2" inline="true"><comment pinned="true" h="80" w="160">Oh my Darling</comment><value name="message"><block type="text" id="3"><field name="TEXT">kjkj</field></block></value></block></value></block></xml>*#*#*/
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="1" inline="false" x="27" y="53"><value name="return"><block type="scriptr_fb_post" id="2" inline="true"><comment pinned="true" h="80" w="160">Oh my Darling
+</comment><value name="message"><block type="text" id="3"><field name="TEXT">kjkj</field></block></value></block></value></block></xml>*#*#*/
 // Oh my Darling
 return facebookPost ('kjkj')			

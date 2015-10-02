7c29f69402887518a654a43ccfc8ebc56add71d5
jmrad <julien.mrad@gmail.com>
2015-10-02 12:04:37 +0300

    ss

---
+++ scripts/blocklyl2.js
@@ -0,0 +1,8 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="text" id="18" x="81" y="13"><field name="TEXT">test</field></block></xml>*#*#*/
+'test';
+			

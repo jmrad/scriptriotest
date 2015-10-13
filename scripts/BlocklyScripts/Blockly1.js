feec66d5f272b246848779477d4e514c98196f57
jmrad <julien.mrad@gmail.com>
2015-10-13 15:11:10 +0300

    Pulling BlocklyScripts for reference purpose.

---
+++ scripts/BlocklyScripts/Blockly1.js
@@ -0,0 +1,8 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_tweet" id="13" inline="true" x="370" y="70"></block></xml>*#*#*/
+tweet (null);
+   							
